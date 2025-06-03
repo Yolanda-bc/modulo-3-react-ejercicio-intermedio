fcb63576eacc31f15de8b10327860efffce5a250
Yolanda-bc <jolandabc89@gmail.com>
2025-06-03 11:23:12 +0200

    add filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,21 @@ import "../styles/App.scss";
 import studentsData from "../data/students.json";
 import Header from "./Header";
 import Main from "./Main";
+import { useState } from "react";
 
 function App() {
-  const students = studentsData.results; 
-   const handleInputFilter = () => {}
+  const [filterName, setFilterName] = useState(""); //variable de estado llamada filterName
+  const handleInputFilter = (ev) => {
+    setFilterName (ev.target.value.toLowerCase());
+  };
+  const students = studentsData.results.filter((student) => student.name.toLowerCase().includes(filterName)
+);
+  
   return (
   
     <div className="app">
       <Header />
-      <Main />
+      <Main handleInputFilter={handleInputFilter}/>
       <table className="table">
         <thead>
           <tr>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ function Main(props) {
   return (
     <main>
         <form className="filters">
-            <label htmlForm="filterName">Filtrar por nombre:</label>
+            <label htmlFor="filterName">Filtrar por nombre:</label>
             <input onInput={props.handleInputFilter} type="text" name="filterName" id="filterName" />
         </form>
     </main>
